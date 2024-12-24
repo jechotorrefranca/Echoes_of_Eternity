d5aca3f5f6c09c288da5b38e31169a32139d42b9
Kouji <Kouji@DESKTOP-VO9O7C0>
2024-12-24 20:07:17 +0800

    test commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        This Marks the Beginning of Echoes of Eternity
-      </h1>
+      <h1 className="text-4xl font-bold text-white">test</h1>
     </div>
   );
 }
