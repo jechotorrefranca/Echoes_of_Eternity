cbed36e0ede77e714c93129a89d633d34fdcb6ae
Kouji <Kouji@DESKTOP-VO9O7C0>
2024-12-24 20:05:06 +0800

    Echoes of Eternity Beginning

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        This Marks the Beginning of Echoes of Eternity
+      </h1>
+    </div>
+  );
+}
+
+export default App;
